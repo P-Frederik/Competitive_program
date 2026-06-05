b20bf4f06b0cb8bdeafadd3f0187536bef92d582
Gia Phát <67.onggiaphat.tqt1@gmail.com>
2026-06-05 08:32:26 +0700

    TS102026

---
+++ tuyen sinh 10ctin 2026/BAI1TS2026.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef vector<bool> vb;
+typedef vector<ll> vl;
+
+#define fp(nm) freopen(nm".INP","r",stdin);freopen(nm".OUT","w",stdout);
+#define boost ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define wrong cout<<"NO"<<endl;
+#define endl '\n';
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+
+const ll MAXN = 1e9+1;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+
+bool BEGIN_ALLOC;
+bool END_ALLOC;
+
+
+void solution(){
+    ll a,b,c,d,k;
+    cin>>a>>b>>c>>d>>k;
+    ll temp1=(a+c)/k;
+    ll temp2=(b+d)/k;
+    ll tmp = max(temp1,temp2)*k;
+    if (tmp>b+d){
+        cout<<-1<<endl;
+        return;
+    }
+    ll tmp2 = b+c;
+    if (tmp==tmp2){
+        cout<<b<<" "<<c<<endl;
+        return;
+    }
+    if (tmp<tmp2){
+        if (tmp-c > d){
+            cout<<tmp-c<<" "<<c<<endl;
+            return;
+        }
+        cout<<c<<" "<<tmp-c<<endl;
+        return;
+    }
+    if (tmp>tmp2){
+        if (tmp-b > d){
+            cout<<tmp-b<<" "<<b<<endl;
+            return;
+        }
+        cout<<b<<" "<<tmp-b<<endl;
+        return;
+    }
+}
+
+int main(){
+    boost
+    // auto startTime = chrono::steady_clock::now();
+    //fp("");
+    ll q=1;
+    // cin>>q;
+    while(q--){
+      solution();
+    }
+//     auto endTime = chrono::steady_clock::now();
+//   cerr << fixed << setprecision(2) << "[Static memory: " << abs((&BEGIN_ALLOC) - (&END_ALLOC)) / (1024.0 * 1024.0) << "mb]\n";
+//   cerr << "[Time elapsed : " << chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count() << "ms]\n";
+    return 0;
+}
+
+/*  Code by FrederikSama1911 - T2 - GD2528
+*/
